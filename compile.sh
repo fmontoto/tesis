74e3d8fea78e5eafd6e1eac862bc803b53dcaf79
fmontoto <fmontotomonroy@gmail.com>
2017-09-20 20:48:02 -0700

    reformat and warning remove

--- compile.sh
+++ compile.sh
@@ -7,12 +7,11 @@ CDW=$(pwd)
 cd ${W_DIR}
 BIN_PATH="/usr/local/texlive/2014/bin/x86_64-darwin/"
 
-pwd
 #pdflatex
-${BIN_PATH}pdflatex -shell-escape -synctex=1 -interaction=nonstopmode ${file}.tex
+${BIN_PATH}pdflatex -shell-escape -synctex=1 -interaction=nonstopmode ${file}.tex > /dev/null 2>&1
 
 #bibtex
-${BIN_PATH}bibtex ${file}.aux
+${BIN_PATH}bibtex `realpath --relative-to=${W_DIR} ${file}.aux`
 
 #index/glossarie
 #${BIN_PATH}makeindex ${file}.idx
@@ -25,7 +24,7 @@ ${BIN_PATH}bibtex ${file}.aux
 #${BIN_PATH}makeindex -s ${file}.ist -t ${file}.alg -o ${file}.acr ${file}.acn
 
 #pdflatex
-${BIN_PATH}pdflatex -shell-escape -synctex=1 -interaction=nonstopmode ${file}.tex
+${BIN_PATH}pdflatex -shell-escape -synctex=1 -interaction=nonstopmode ${file}.tex > /dev/null 2>&1
 ${BIN_PATH}pdflatex -shell-escape -synctex=1 -interaction=nonstopmode ${file}.tex
 
 cd ${CDW}
